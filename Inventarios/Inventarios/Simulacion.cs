f57636dd00a1c5dea9fa2c57419392257bdf8dbc
ofsaga <magt.8874@gmail.com>
2014-01-30 17:55:32 -0600

    Se agrego la funcion para la simulacion de una venta

--- Inventarios/Inventarios/Simulacion.cs
+++ Inventarios/Inventarios/Simulacion.cs
@@ -7,10 +7,52 @@ namespace Inventarios
 {
     class Simulacion
     {
-
         public void Iniciar()
         {
+            int numSer = 250; //200-250
+            for (int i = 0; i < numSer; i++)
+            {
+                crearVenta();
+                //
+            }
+        }
 
+        public void crearVenta()
+        {
+            int idEmp;
+            int totalDeta;
+            int cantPro;
+            int idPro;
+            int numPro;
+            float precio,subT,totalNeto,IVA;
+            List<List<object>> detalle = new List<List<object>>();
+
+            //Detalle Venta
+            numPro = 10;
+            totalDeta = 5;//1-5
+            subT = 0;
+            totalNeto = 0;
+            for (int i = 0; i<totalDeta ; i++)
+            {
+                idPro = 1;//
+                //Sacar precio
+                precio = 2.5f;
+                cantPro = 2;//1-5
+                detalle[i] = new List<object>();
+
+                detalle[i].Add(idPro);
+                detalle[i].Add(precio);
+                detalle[i].Add(cantPro);
+
+                subT += precio*cantPro;
+                totalNeto += subT;
+            }
+
+            IVA = totalNeto*0.16f;
+            idEmp = 1;//Todos los empleados
+            totalNeto += IVA;
+            //INSERT INTO VENTAS (fecha,id_empleado,iva,total)VALUES (FECHA,idEmp,IVA,totalNeto);
+            
         }
     }
 }
