1176616b8db4764836f8aa9695f65b21603f5ec4
ofsaga <magt.8874@gmail.com>
2014-02-12 16:23:27 -0600

    Se agrego el campo stock_maximo

--- Scripts/disparadores.sql
+++ Scripts/disparadores.sql
@@ -4,5 +4,14 @@ DROP TRIGGER IF EXISTS actualizaStock;
 CREATE TRIGGER actualizaStock AFTER INSERT ON detalle_venta 
 FOR EACH ROW
 	UPDATE inventarios SET stock = stock - NEW.cantidad 
-	WHERE inventarios.id_articulo = NEW.id_articulo
+	WHERE inventarios.id_articulo = NEW.id_articulo;
 
+
+CREATE TRIGGER crearOrdenProduccion AFTER UPDATE ON inventarios
+FOR EACH ROW
+	
+	id_empleado 
+	id_articulo 
+	fecha_inicio 
+	fecha_entrega
+	cantidad RaND()
