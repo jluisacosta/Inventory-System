3182926d97e999ee0a8a015b2943a5ec6e199596
JLuis <jluis.acosta@alumnos.uaslp.edu.mx>
2014-02-17 17:58:29 -0600

    Se hizo la mini aplicacion de la insercion de materiapor producto

--- Scripts/datos_materia_prima.sql
+++ Scripts/datos_materia_prima.sql
@@ -1,63 +1,60 @@
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Leche','Ultrapasteurizada',12.90,50,10,40,'2013/01/01','MateriaPrima','Litros');
 
-
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Harina','Harina blanca',11.20,50,10,30,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Huevo','18 piezas',32.20,50,15,40,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Crema chatillí','5kg por carton',75.00,40,10,25,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Azucar','Azucar morena agranel',14.90,80,20,90,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Mantequilla','Con sal', 93.00,50,30,80,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
-				(null,'MP','Levadura','Royal',5,2,6,'2013/01/01','MateriaPrima','Kilogramos');
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
+				(null,'MP','Levadura','Royal',17.45,5,2,6,'2013/01/01','MateriaPrima','Kilogramos');
 
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
+				(null,'MP','Cacao','Hershys',45.76,5,2,6,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
-				(null,'MP','Cacao','Royal',5,2,6,'2013/01/01','MateriaPrima','Kilogramos');
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
+				(null,'MP','Chocolate','Amargo',60.00,5,2,6,'2013/01/01','MateriaPrima','Kilogramos');
 
-
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
-				(null,'MP','Chocolate','Royal',60.00,5,2,6,'2013/01/01','MateriaPrima','Kilogramos');
-
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Nuez','Moscada',72.00,8,3,6,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Vainilla','Extracto',33.80,5,1,2,'2013/01/01','MateriaPrima','Litros');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Piña','En almibar',38.89,4,1,2,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Durazno','En almibar',43.50,4,1,2,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Fresa','Pico de pàjaro',34.67,12,4,8,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Queso crema','-------',79.00,8,3,6,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Manzana','Red delicious',18.30,6,2,4,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Menta','Extracto',130.00,2,1,1,'2013/01/01','MateriaPrima','Litros');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Azùcar glass','Ultrafina',13.45,4,2,2,'2013/01/01','MateriaPrima','Kilogramos');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Limon','Concentrado',65.00,3,1,2,'2013/01/01','MateriaPrima','Litros');
 
-insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,tipo_articulo,unidad) values
+insert into Inventarios(id_proveedor,categoria,nombre,descripcion,precio,stock,stock_minimo,stock_maximo,fecha_ingreso,tipo_articulo,unidad) values
 				(null,'MP','Semilla de amapola','Importada',67.80,2,1,1,'2013/01/01','MateriaPrima','Kilogramos');
