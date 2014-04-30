13a6dc3223778ae38dda76ea1145ccd20314c32d
ofsaga <magt.8874@gmail.com>
2014-04-30 16:40:46 -0500

    Se agrego la orden produccion

---
+++ Scripts/Reportes/orden_produccion.sql
@@ -0,0 +1,11 @@
+
+select op.id_orden_produccion,op.fecha_inicio, e.id_empleado, e.nombre as empleado, p.nombre as produto,op.cantidad,
+mp.id_materia, mp.nombre,mp.stock,op.cantidad * mpp.cantidad as cant, mp.unidad
+from ordenes_produccion op, productos p, materias_primas mp, materiaporproducto mpp, empleados e
+where op.id_producto = p.id_producto
+and p.id_producto = mpp.id_producto
+and mpp.id_materia = mp.id_materia
+and op.id_empleado = e.id_empleado
+and op.id_orden_produccion =  50000
+
+
