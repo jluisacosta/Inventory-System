f19e69d40bb0418306bd21869a9e5fdd9cff6f2e
ofsaga <magt.8874@gmail.com>
2014-05-08 16:13:04 -0500

    Se creo el catalogo de Empleados

--- Scripts/Reportes/Especiales/ventas_emp.sql
+++ Scripts/Reportes/Especiales/ventas_emp.sql
@@ -6,7 +6,8 @@ and v.fecha >= '2004/01/01'
 and v.fecha <= '2004/01/01'
 group by e.id_empleado
 
-select * from proveedores
+select * from usuarios
 
+select * from tipos_empleados
 
 update Clientes set ciudad = 'Mexico', cp = 12135 where id_cliente = 300
